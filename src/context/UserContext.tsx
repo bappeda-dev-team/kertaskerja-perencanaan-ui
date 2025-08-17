4fd6a888655d5bd75ebfe1e25b45efa99bf559ec
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-08-18 00:06:40 +0700

    fix loading state tidak kelar kelar

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -56,6 +56,7 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
             if (!res.ok) throw new Error("Unauthorized");
             const data: User = await res.json();
             setUser(data);
+            setLoading(false);
         } catch {
             setUser(null);
         } finally {
